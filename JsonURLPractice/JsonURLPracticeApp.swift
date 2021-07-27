b84b5614dfe712390d2685314341c6258d5ce3b6
Promal B <promal.barua.cse@gmail.com>
2021-07-28 04:15:33 +0600

    Initial Commit

---
+++ JsonURLPractice/JsonURLPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonURLPracticeApp.swift
+//  JsonURLPractice
+//
+//  Created by Promal on 28/7/21.
+//
+
+import SwiftUI
+
+@main
+struct JsonURLPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
